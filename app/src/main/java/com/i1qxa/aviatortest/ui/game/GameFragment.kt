8e3ce20cfa50f190c8b58394a2c06f0fdc8e0dbe
Кукса <kuiv1989@gmail.com>
2024-07-25 17:35:22 +0300

    add loading screen

---
+++ app/src/main/java/com/i1qxa/aviatortest/ui/game/GameFragment.kt
@@ -0,0 +1,31 @@
+package com.i1qxa.aviatortest.ui.game
+
+import androidx.fragment.app.viewModels
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.i1qxa.aviatortest.R
+
+class GameFragment : Fragment() {
+
+    companion object {
+        fun newInstance() = GameFragment()
+    }
+
+    private val viewModel: GameViewModel by viewModels()
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        // TODO: Use the ViewModel
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        return inflater.inflate(R.layout.fragment_game, container, false)
+    }
+}
---
+++ app/src/main/java/com/i1qxa/aviatortest/ui/game/GameViewModel.kt
@@ -0,0 +1,7 @@
+package com.i1qxa.aviatortest.ui.game
+
+import androidx.lifecycle.ViewModel
+
+class GameViewModel : ViewModel() {
+    // TODO: Implement the ViewModel
+}
